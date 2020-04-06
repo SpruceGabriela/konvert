403f268ac03dd37c1cf11ca48a788e2cfa67d0a6
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2020-04-06 17:23:53 -0300

    making some global style changes

--- src/components/Conversor/Conversor.js
+++ src/components/Conversor/Conversor.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './Conversor.css'
 import Cotation from '../Cotation/Cotation';
 import Form from '../Form/Form';
+import Result from '../Result/Result';
 
 export default function Conversor() {
     return (
@@ -12,7 +13,7 @@ export default function Conversor() {
                 <Form/>
             </div>
             <div className="result-container">
-
+                <Result/>
             </div>
         </div>
     );
