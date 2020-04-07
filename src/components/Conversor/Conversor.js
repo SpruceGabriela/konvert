ad0680c22fa8ee3c56d586f99758e34e2764e118
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2020-04-06 22:22:32 -0300

    displaying dollar and real values

--- src/components/Conversor/Conversor.js
+++ src/components/Conversor/Conversor.js
@@ -6,10 +6,18 @@ import Form from '../Form/Form';
 import Result from '../Result/Result';
 
 import economyService from '../../services/economyService';
+import Input from '../Input/Input';
+import RadioButton from '../RadioButton/RadioButton';
+import Button from '../Button/Button';
+import Usa from '../../assets/usa.png';
+import Percent from '../../assets/percent.png'
 
 export default function Conversor() {
     const [currency, setCurrency] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [tax, setTax] = useState(0);
+    const [amount, setAmount] = useState(0);
+    const [stateTax, setStateTax] = useState(0);
 
     useEffect(function() {
         async function getCurrency(){
@@ -23,16 +31,44 @@ export default function Conversor() {
         getCurrency();
     },[])
 
+    const amountChangedHandler = e => {
+        setAmount(e.target.value);
+    }
+
+    const iofChangedHandler = e => {
+        setStateTax(e.target.value);
+    }
+
     return (
         <div className="conversor-container">
             {loading ? <div>loading...</div> : (
                 <>
                     <div className="conversor-content">
                         <Cotation cotation={currency.bid}/>
-                        <Form/>
+                        <Form>
+                            <div className="input-text">
+                                <img src={Usa} alt="Bandeira dos Estados Unidos"/>
+                                <Input placeholder="Valor em Dólar" onChange={amountChangedHandler}/>
+                            </div>
+                            <div className="input-text">
+                                <img src={Percent} alt="Taxa de estado"/>
+                                <Input placeholder="Taxa de estado" onChange={iofChangedHandler}/>
+                            </div>
+                            <div className="input-group">
+                                <RadioButton name="payment" value="1.1" id="money" onChange={e => setTax(e.target.value)}/>
+                                <label htmlFor="money">Dinheiro</label>
+                                <RadioButton name="payment" value="6.4" id="card" onChange={e => setTax(e.target.value)}/>
+                                <label htmlFor="card">Cartão</label>
+                            </div>
+                        </Form>
                     </div>
                     <div className="result-container">
-                        <Result cotation={currency.bid}/>
+                        <Result
+                            cotation={+currency.bid}
+                            tax={+tax}
+                            amount={+amount}
+                            stateTax={+stateTax}
+                            />
                     </div>
                 </>
             )}
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -7,30 +7,44 @@ import Brl from '../../assets/brazil.png';
 import Arrow from '../../assets/change.png';
 
 export default function Result(props) {
+    const {amount, stateTax, cotation, tax} = props;
+
+    function getDollarWithTax(){
+        return amount + (amount * stateTax)/100;
+    }
+
+    function getRealWithoutTax(){
+        return amount * cotation;
+    }
+
+    function getRealWithTax(){
+        return (amount + (amount * tax)/100) * (cotation +(cotation * tax)/100)
+    }
+    
     return (
         <div className="info-container">
             <section className="information-group">
                 <h1>IOF</h1>
                 <div className="information-total">
                    <div className="currency-title">
-                    <img src={Usa}/>
+                    <img src={Usa} alt="Bandeira dos Estados Unidos"/>
                     <p>Total em USD:</p>
                    </div>
 
                    <div className="totals">
-                        <p>Valor sem imposto</p>
-                        <p>Valor com imposto</p>
+                        <p>Valor sem imposto: {amount}</p>
+                        <p>Valor com imposto: {getDollarWithTax()}</p>
                     </div>
                 </div>
-                <img src={Arrow}/>
+                <img src={Arrow} alt="Conversão"/>
                 <div className="information-total">
                    <div className="currency-title">
-                    <img src={Brl}/>
+                    <img src={Brl} alt="Bandeira do Brasil"/>
                     <p>Total em BRL:</p>
                    </div>
                    <div className="totals">
-                        <p>Valor sem imposto</p>
-                        <p>Valor com imposto</p>
+                        <p>Valor sem imposto: {getRealWithoutTax()}</p>
+                        <p>Valor com imposto: {getRealWithTax()}</p>
                     </div>
                 </div>
             </section>
